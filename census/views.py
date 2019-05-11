2541496089ef9bbb8e37ba37c3ceaa58c257860f
tangoyankee <miller.tim108@gmail.com>
2019-05-11 16:26:33 -0700

    staff only page to view and approve pending events

--- census/views.py
+++ census/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import Event
 
 def index(request):
     return render(request, 'index.html')
 
+@staff_member_required
 def pending_events(request):
-    return render(request, 'pending_events.html')
+    events = Event.objects.filter(approval_status="PENDING")
+    return render(request, 'pending_events.html', {'events': events})
