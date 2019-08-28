4929b26df7b849200bb61f4a99593f79d2ad7ca1
Mike Ubell <mike.ubell@gmail.com>
2019-08-27 20:43:21 -0700

    WIP

--- census/views.py
+++ census/views.py
@@ -44,3 +44,22 @@ def export_events(request):
 
 def index(request):
     return render(request, 'index.html')
+
+from .forms import EventForm
+from django.http import HttpResponseRedirect
+
+def add_event(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EventForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/submit/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = EventForm()
+
+    return render(request, 'event.html', {'form': form})
