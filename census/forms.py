2adec6f08a59fa37ebe58017fd880532c414f7ce
Mike Ubell <mike.ubell@gmail.com>
2019-08-31 16:42:27 -0700

    Add submit page.  Change from datetime to time.

--- census/forms.py
+++ census/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
+from django  import forms
+from datetime import datetime
 from .models import Event
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
+        widgets = {
+                'start_time': forms.TimeInput(attrs={'type': 'time'}),
+                'end_time': forms.TimeInput(attrs={'type': 'time'})
+        }
         exclude = ['approval_status']
 
