84cb5a0cc252935bc8774952cdd124af5a57783f
Edmundo Martinez <edmundonm@gmail.com>
2019-11-10 13:27:54 -0800

    Remove unused code

--- census/forms.py
+++ census/forms.py
@@ -11,11 +11,3 @@ class EventForm(ModelForm):
                 'end_time': forms.TimeInput(attrs={'type': 'time'})
         }
         exclude = ['approval_status', 'lon', 'lat']
-
-    def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            if visible.name in ['title', 'event_type', 'location', 'organization_name']:
-                visible.field.widget.attrs['class'] = 'usa-input'
-            elif visible.name in ['description']:
-                visible.field.widget.attrs['class'] = 'usa-textarea'
